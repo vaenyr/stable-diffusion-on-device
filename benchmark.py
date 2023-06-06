a0838d7953345622a4b5ec0ee8a074898cfae4af
Łukasz Dudziak <l.dudziak@samsung.com>
2023-06-06 17:42:58 +0100

    Parallelize dlc conversion and benchmarking

--- benchmark.py
+++ benchmark.py
@@ -4,7 +4,6 @@ import argparse
 import logging
 from pathlib import Path
 from pprint import pprint
-from functools import cmp_to_key
 
 from natsort import natsorted
 
@@ -25,7 +24,7 @@ if regex:
 logging.basicConfig(level=logging.DEBUG if debug else logging.INFO)
 
 
-bench = snpe.SnpeBenchmark(devices='S23+', runtime='dsp', quantize=8)
+bench = snpe.SnpeBenchmark(devices=['S23', 'S23+', 'S23 Ultra'], runtime='dsp', quantize=8)
 
 dlc_folder = Path(__file__).parent.joinpath('dlc')
 results_folder = Path(__file__).parent.joinpath('results')
@@ -33,44 +32,57 @@ results_folder = Path(__file__).parent.joinpath('results')
 ok_models = []
 failed_models = []
 total = 0
+
+
 candidates = natsorted(dlc_folder.rglob('*.int8.dlc'), key=lambda p: f'_{str(p).count(os.path.sep)}{str(p)}') # prefer less nested files first by appending a number of "/" in the path at the beginning
-for qmodel in candidates:
-    if regex:
-        if not regex.search(str(qmodel)):
-            continue
+with bench.async_context():
+    jobs = []
+    for qmodel in candidates:
+        if regex:
+            if not regex.search(str(qmodel)):
+                continue
 
-    part = qmodel.relative_to(dlc_folder)
-    result_file = results_folder.joinpath(part).with_suffix('.txt')
-    error_file = result_file.with_suffix('.error')
-    result_file.parent.mkdir(parents=True, exist_ok=True)
+        part = qmodel.relative_to(dlc_folder)
 
-    if result_file.exists():
-        result_file.unlink()
-    if error_file.exists():
-        error_file.unlink()
+        print(f'Scheduling {part}... ', flush=True)
+        future = bench.run_async(qmodel, convert=False)
+        jobs.append((part, future))
 
     try:
-        print(f'Running {part}... ', end='', flush=True)
-        result = bench.run(qmodel, convert=False)
-    except Exception:
-        print('Error')
-        failed_models.append(part)
-        import traceback
-        with error_file.open('w') as f:
-            traceback.print_exc(file=f)
-    else:
-        print('Success', result['latency'])
-        ok_models.append(part)
-        total += result['latency']
-        with result_file.open('w') as f:
-            print(part, file=f)
-            pprint(result, stream=f)
-            print(file=f)
+        for part, job in jobs:
+            result_file = results_folder.joinpath(part).with_suffix('.txt')
+            error_file = result_file.with_suffix('.error')
+            result_file.parent.mkdir(parents=True, exist_ok=True)
 
-if failed_models:
-    print('Failed models:')
-    for p in failed_models:
-        print('   ', p)
-print('Successes:', len(ok_models), 'Failures:', len(failed_models))
-if len(ok_models) > 1:
-    print('Total latency of successful models:', total)
+            if result_file.exists():
+                result_file.unlink()
+            if error_file.exists():
+                error_file.unlink()
+
+            try:
+                result = job.result()
+            except Exception:
+                print(part, 'Error')
+                failed_models.append(part)
+                import traceback
+                with error_file.open('w') as f:
+                    traceback.print_exc(file=f)
+            else:
+                print(part, 'Success', result['latency'])
+                ok_models.append(part)
+                total += result['latency']
+                with result_file.open('w') as f:
+                    print(part, file=f)
+                    pprint(result, stream=f)
+                    print(file=f)
+
+        if failed_models:
+            print('Failed models:')
+            for p in failed_models:
+                print('   ', p)
+        print('Successes:', len(ok_models), 'Failures:', len(failed_models))
+        if len(ok_models) > 1:
+            print('Total latency of successful models:', total)
+    finally:
+        for _, job in jobs:
+            job.cancel()
--- todlc.py
+++ todlc.py
@@ -2,7 +2,9 @@ import os
 import re
 import argparse
 import logging
+import multiprocessing
 from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor, wait
 
 from natsort import natsorted
 
@@ -12,7 +14,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--debug', action='store_true')
 parser.add_argument('--regex', type=str, default=None)
 parser.add_argument('--force', action='store_true')
-parser.add_argument('--path', type=str, default=str(Path(__file__).parents[1].joinpath('stable-diffusion', 'onnx')))
+parser.add_argument('--path', type=str, default=str(Path(__file__).absolute().parents[1].joinpath('stable-diffusion', 'onnx')))
 
 args = parser.parse_args()
 
@@ -30,42 +32,59 @@ models_folder = Path(args.path)
 output_folder = Path(__file__).parent.joinpath('dlc')
 output_folder.mkdir(parents=True, exist_ok=True)
 
+
+def convert_onnx(onnx_file):
+    part = onnx_file.relative_to(models_folder)
+    dlc_fp32 = output_folder.joinpath(part).with_suffix('.dlc')
+    dlc_int8 = dlc_fp32.with_suffix('.int8.dlc')
+    dlc_fp32.parent.mkdir(parents=True, exist_ok=True)
+    if not onnx_file.exists():
+        print('Error: source model file does not exist!', onnx_file)
+        return
+
+    if not dlc_fp32.exists() or args.force:
+        print('Attempting ONNX -> DLC (fp32) conversion for part:', part)
+        try:
+            dlcc.compile(onnx_file, model_type='onnx-file', output_file=dlc_fp32)
+        except Exception:
+            print('Error occurred while converting! Model will be skipped!', part)
+            import traceback
+            traceback.print_exc()
+            return
+
+    print('waiting...')
+    input()
+    if not dlc_int8.exists() or args.force:
+        print('Attempting DLC (fp32) -> DLC (int8) quantization for part:', part)
+        try:
+            dlcc.quantize(dlc_fp32, precision=8, output_file=dlc_int8)
+        except Exception:
+            print('Error occurred while quantizing! Model will be skipped!', part)
+            import traceback
+            traceback.print_exc()
+            return
+
+    if dlc_int8.exists():
+        print('Model successfully converted and quantized!', part)
+    else:
+        print('No error has been reported but quantized model file does not exist! Check logs.', part)
+
+
 with dlcc.keepfiles(debug):
-    candidates = natsorted(models_folder.rglob('*.onnx'), key=lambda p: f'_{str(p).count(os.path.sep)}{str(p)}')
-    for onnx_file in candidates:
-        if regex:
-            if not regex.search(str(onnx_file)):
-                continue
+    with ThreadPoolExecutor(max_workers=multiprocessing.cpu_count()) as pool:
+        try:
+            candidates = natsorted(models_folder.rglob('*.onnx'), key=lambda p: f'_{str(p).count(os.path.sep)}{str(p)}')
+            jobs = []
+            for onnx_file in candidates:
+                if regex:
+                    if not regex.search(str(onnx_file)):
+                        continue
 
-        part = onnx_file.relative_to(models_folder)
-        dlc_fp32 = output_folder.joinpath(part).with_suffix('.dlc')
-        dlc_int8 = dlc_fp32.with_suffix('.int8.dlc')
-        dlc_fp32.parent.mkdir(parents=True, exist_ok=True)
-        if not onnx_file.exists():
-            print('Error: source model file does not exist!', onnx_file)
-            continue
+                #jobs.append(pool.submit(convert_onnx, onnx_file))
+                convert_onnx(onnx_file)
 
-        if not dlc_fp32.exists() or args.force:
-            print('Attempting ONNX -> DLC (fp32) conversion for part:', part)
-            try:
-                dlcc.compile(onnx_file, model_type='onnx-file', output_file=dlc_fp32)
-            except Exception:
-                print('Error occurred! Model will be skipped!')
-                import traceback
-                traceback.print_exc()
-                continue
-
-        if not dlc_int8.exists() or args.force:
-            print('Attempting DLC (fp32) -> DLC (int8) quantization for part:', part)
-            try:
-                dlcc.quantize(dlc_fp32, precision=8, output_file=dlc_int8)
-            except Exception:
-                print('Error occurred! Model will be skipped!')
-                import traceback
-                traceback.print_exc()
-                continue
-
-        if dlc_int8.exists():
-            print('Model successfully converted and quantized!', part)
-        else:
-            print('No error has been reported but quantized model file does not exist! Check logs.', part)
+            wait(jobs)
+        finally:
+            for job in jobs:
+                job.cancel()
+            pool.shutdown(wait=False)
